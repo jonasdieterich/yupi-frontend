c3fb72cd927521a47a3da6f670c6810deb85fb8d
Sam Warner <sam_warner@talktalk.net>
2024-02-03 16:17:19 +0000

    Fix issue where selected words in check-in aren't highlighted

--- src/component/WordSelectorCheckInComponent.jsx
+++ src/component/WordSelectorCheckInComponent.jsx
@@ -7,8 +7,8 @@ const WordSelectorCheckInComponent = ({moodOptions, moods, setMoods, setCheckInS
             <div className="flex flex-wrap items-start justify-center p-5 py-10">
             {moodOptions.map(moodOption => {
                 return (
-                    <div className={`m-2 bg-white text-gray-800 font-semibold py-2 px-4 border border-purple-400 rounded
-                                    shadow ${(!moods.includes(moodOption) ? 'hover:bg-purple-100' : 'bg-purple-400')}`}
+                    <div className={`m-2 text-gray-800 font-semibold py-2 px-4 border border-purple-400 rounded
+                                    shadow ${(!moods.includes(moodOption) ? 'bg-white hover:bg-purple-100' : 'bg-purple-400')}`}
                         onClick={() => {
                             if (!moods.includes(moodOption) && moods.length < 3) {
                                 setMoods(moods => [...moods, moodOption])
