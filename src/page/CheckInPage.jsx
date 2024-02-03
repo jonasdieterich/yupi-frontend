f351086526754fbcbe0c13c3cbad4d08785b0169
Sam Warner <sam_warner@talktalk.net>
2024-02-03 16:42:55 +0000

    Add ability to move backwards in check-in

--- src/page/CheckInPage.jsx
+++ src/page/CheckInPage.jsx
@@ -2,19 +2,28 @@ import React, {useState} from "react";
 import MoodSelectorCheckInContainer from "../container/MoodSelectorCheckInContainer";
 import WordSelectorCheckInContainer from "../container/WordSelectorCheckInContainer";
 import WellbeingActivitySelectorCheckInContainer from "../container/WellbeingActivitySelectorCheckInContainer";
+import FeatherIcon from "feather-icons-react";
 
 const CheckInPage = () => {
     const [checkInStage, setCheckInStage] = useState(0);
 
     return (
-        <div className="flex flex-col justify-center items-center text-center">
-            {
+        <div className="flex flex-col">
+            { checkInStage > 0 && (
+                <div className="flex flex-row p-4 pl-0" onClick={() => {setCheckInStage(prev => prev - 1)}}>
+                    <FeatherIcon icon="arrow-left" />
+                    Back
+                </div>
+            )}
+            <div className="flex flex-col justify-center items-center text-center m-auto">
                 {
-                    0: <MoodSelectorCheckInContainer setCheckInStage={setCheckInStage}/>,
-                    1: <WordSelectorCheckInContainer setCheckInStage={setCheckInStage}/>,
-                    2: <WellbeingActivitySelectorCheckInContainer setCheckInStage={setCheckInStage}/>
-                }[checkInStage]
-            }
+                    {
+                        0: <MoodSelectorCheckInContainer setCheckInStage={setCheckInStage}/>,
+                        1: <WordSelectorCheckInContainer setCheckInStage={setCheckInStage}/>,
+                        2: <WellbeingActivitySelectorCheckInContainer setCheckInStage={setCheckInStage}/>
+                    }[checkInStage]
+                }
+            </div>
         </div>
     );
 }
